b270c778aaee4da0b6eaf3741f75711dc2b85303
Sonya <gayanovvas@mail.ru>
2022-12-02 20:23:13 +0300

    задача 8

---
+++ HomeWork/HW1/З8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+
+while(i < N)
+{
+    if(i%2==0)
+    { 
+      Console.Write(i);
+    }
+
+    else 
+    {
+        i++;
+    }
+}
+
+ Console.Write(i);
