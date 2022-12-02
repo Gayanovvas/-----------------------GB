f6fabaf30c06da204ef6dab861b8a165480eaaa7
Sonya <gayanovvas@mail.ru>
2022-12-02 18:16:49 +0300

    задача 2

---
+++ HomeWork/HW1/Задача 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите число a ");
+Console.Write("Введите число b ");
+
+double numberA = double.Parse(Console.ReadLine());
+double numberB = double.Parse(Console.ReadLine());
+
+double max = numberA;
+
+if(numberA > numberB)
+    {
+        numberA = max;
+    }
+
+else 
+{
+    numberB = max;
+}
+
+Console.Write("max= ");
+Console.WriteLine(max);
