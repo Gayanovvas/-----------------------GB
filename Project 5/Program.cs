94ae7b1871d67c40bbf83c1d478e4126aab6f6be
Sonya <gayanovvas@mail.ru>
2022-12-02 13:55:42 +0300

    программа

--- Project 5/Program.cs
+++ Project 5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username == "Masha");
+{
+    Console.WriteLine("Ura, eto zhe MASHA");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(usermane);
+};
