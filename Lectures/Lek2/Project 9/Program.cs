54f7e8317a59893065547cf1ee685585b371d7e0
Sonya <gayanovvas@mail.ru>
2022-12-03 12:53:58 +0300

    задание 2, лекция 2

--- Lectures/Lek2/Project 9/Program.cs
+++ Lectures/Lek2/Project 9/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, arg2, arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result; 
+}
+int[] array = {11, 21, 31, 41, 51, 61, 71, 81, 91};
+array [0]=12;
+Console.WriteLine(array[4]);
+
+
