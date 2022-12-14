41e0afaced6fb0f31b39cdd130287823b1ccf495
Sonya <gayanovvas@mail.ru>
2022-12-14 15:49:50 +0300

    задание 34

---
+++ HomeWork/HW5/Задача 34/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int CountPlus (int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] %2==0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+
+int[] CreateMassive(int count, int begin, int end)
+{
+    int[] array = new int[count];
+    for(int i = 0; i < count; i++) 
+    {
+        array[i] = new Random().Next(begin, end+1);
+    }
+    return array;
+}
+
+//Создаем массив
+int[] array = CreateMassive(10,100,999);
+Console.WriteLine(String.Join(", ", array));
+//Ищем число четных
+int count = CountPlus (array);
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
