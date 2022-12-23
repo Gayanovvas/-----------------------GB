a82a021e564d3b67da8252e8429ff13d7ce27221
Sonya <gayanovvas@mail.ru>
2022-12-23 14:10:17 +0300

    дз №6

---
+++ HomeWork/HW6/Задача 41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountElements(int[] array)
+{
+    int result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+int[] EnterData(string text)
+{
+    Console.WriteLine(text);
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);;
+    return arr;
+}
+
+// Ввести массив
+int[] array = EnterData("Введите массив: ");
+// Посчитать положительные числа
+int number = CountElements(array);
+// Вывод данных
+Console.WriteLine("Положительных чисел в заданном массиве: " + number);
---
+++ HomeWork/HW6/Задача 43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Ввести данные
+int b1 = EnterData("Введите b1: ");
+int k1 = EnterData("Введите k1: ");
+int b2 = EnterData("Введите b2: ");
+int k2 = EnterData("Введите k2: ");
+// Построить прямые и найти точку пересечения 
+double x = (b1-b2)/(k1-k2);
+double y = k1 * x + b1;
+// Вывод данных
+Console.WriteLine($"Прямые пересекаются в точке: ({x};{y})");
