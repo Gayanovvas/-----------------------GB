7da700ab3b70cf5b290a873330edfa33a5ca2d2c
Sonya <gayanovvas@mail.ru>
2022-12-23 16:44:59 +0300

    дз №7

---
+++ HomeWork/HW7/Задача 47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j]+" "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(begin, end+1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int m = EnterData("Введите количество строк");
+int n = EnterData("Введите количество столбцов");
+int[,] matrix = FillArray(m, n, -100, 100);
+PrintArray(matrix);
---
+++ HomeWork/HW7/Задача 52/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+double ArithmeticMean(int[,] array, int rows, int columns)
+{
+    int count = 0;
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            count = (count + array[i,j])/3;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j]+" "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(begin, end+1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int m = EnterData("Введите количество строк");
+int n = EnterData("Введите количество столбцов");
+int[,] matrix = FillArray(m, n, -100, 100);
+PrintArray(matrix);
+
+double x = ArithmeticMean(matrix);
+Console.WriteLine(x);
