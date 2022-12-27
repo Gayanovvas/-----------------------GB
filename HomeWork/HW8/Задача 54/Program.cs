0baa4c109ce1d7e028eb2b527aee9b8d3192e011
Sonya <gayanovvas@mail.ru>
2022-12-27 10:28:31 +0300

    решение первых двух зачач

---
+++ HomeWork/HW8/Задача 54/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void SortArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for(int c = 0; c < array.GetLength(1); c++)
+        {
+            for(int j = 0; j < array.GetLength(1)-1; j++)
+            {
+                int temp = array[i,j];
+                if(array[i,j]<array[i,j+1])
+                {
+                    array[i,j+1] = temp;
+                    array[i,j] = array[i,j+1];
+                    temp = array[i,j];
+                }
+                else j++;
+            }
+            Console.Write(array[i,c]+" "); 
+        }
+    }    
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j]+" "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(begin, end+1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Запрос строк
+int rows = EnterData("Введите количество строк:");
+// Запрос столбцов
+int columns = EnterData("Введите количество столбцов:");
+// Создание массива и заполнение
+int[,] matrix = FillArray(rows, columns, -10, 10);
+// Вывод массива
+PrintArray(matrix);
+// Сортировка
+SortArray(matrix);
+
---
+++ HomeWork/HW8/Задача 56/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int FindMinSum(int[,] arr)
+{
+    int sum = 0;
+    int min = 0;
+    int row = 1;
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(j < arr.GetLength(1)-1)
+            {
+                sum = arr[i,j]+arr[i,j+1];
+            }
+            if(i==0) min = sum;
+            if(sum < min)
+            {
+                min = sum;
+                row = i++;
+            }
+        }
+    }
+    return row;
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j]+" "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(begin, end+1);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Запрос строк
+int rows = EnterData("Введите количество строк:");
+// Запрос столбцов
+int columns = EnterData("Введите количество столбцов:");
+// Создание массива и заполнение
+int[,] matrix = FillArray(rows, columns, -10, 10);
+// Вывод массива
+PrintArray(matrix);
+// Посчитать сумму элементов в каждой строке и сравнить
+int MinRow = FindMinSum (matrix);
+Console.WriteLine("Минимальная сумма элементов в " + MinRow + " строке");
