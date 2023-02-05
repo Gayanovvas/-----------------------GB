ff964e627769a89adf9c6398f6e59ab252a905f8
Sonya <gayanovvas@mail.ru>
2023-02-05 15:11:05 +0300

    итоговый проект

---
+++ Итоговый проект/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string String = Console.ReadLine();
+M = String.Split(' ');
+var result = new string[M .Length];
+var Size = 0;
+foreach (var value in M )
+{
+    if (value.Length <= 3)
+    {
+        result[Size] = value;
+        Size++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, Size));
+Console.ReadKey(true);
